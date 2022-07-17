4c16d609de8d59ce687c19667c067372c16e800e
João Lima <jg.limamarinho202@gmail.com>
2022-07-17 16:44:50 -0300

    🐛 Fix: DownloadFile bug & Image attached bug

--- templates/static/plataforma/js/markup.js
+++ templates/static/plataforma/js/markup.js
@@ -1,43 +1,40 @@
 function downloadFile(href, file) {
-    let a = document.createElement('a');
-    a.style = 'display: none;';
-    a.href = href;
-    a.download = file;
-    document.body.appendChild(a);
+  let a = document.createElement("a");
+  a.style.display = "none";
+  a.href = href;
+  a.download = file;
+  a.target = "_blank";
+  document.body.appendChild(a);
+  a.click();
 
-    console.log(a);
-
-    a.addEventListener('click', event => {event.preventDefault();});
-
-    a.click();
-    document.body.removeChild(a);
+  console.log("Downloaded");
 }
 
 function sendMarkup() {
-    let plano = document.querySelector('#plano-alimentar').outerHTML;
-    let btn = document.querySelector('#export');
+  let plano = document.querySelector("#plano-alimentar").outerHTML;
+  let btn = document.querySelector("#export");
 
-    let nomePaciente = btn.name;
+  let nomePaciente = btn.name;
 
-    let data = {
-        "html": plano,
-        "name": nomePaciente
-    };
-    
-    fetch('http://127.0.0.1:8000/api/plano_alimentar', {
-        method: "POST",
-        body: JSON.stringify(data),
-        headers: {
-            "Content-Type": "application/json",
-            "Accept": "application/json"
-        }
-    })
-    .then(response => response.json())
-    .then(response => {
-        if(response.success) {
-            downloadFile(response.body, `${data.name}.pdf`);
-        } else {
-            console.log('fudeu');
-        }
+  let data = {
+    html: plano,
+    name: nomePaciente,
+  };
+
+  fetch("http://127.0.0.1:8000/api/plano_alimentar", {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+  })
+    .then((response) => response.json())
+    .then((response) => {
+      if (response.success) {
+        downloadFile(response.body, `${data.name}.pdf`);
+      } else {
+        console.log("fudeu");
+      }
     });
 }
